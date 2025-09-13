77b946608f7c86d25e76dc1d9e673f4e9e3c3ee2
IUNUM <h2410042@nushigh.edu.sg>
2025-09-13 22:05:16 +0800

    i hope it kinda works

--- core.gd
+++ core.gd
@@ -2,8 +2,17 @@ extends RigidBody2D
 
 @export var THRUST: float = 10000
 
-func _integrate_forces(state: PhysicsDirectBodyState2D) -> void:
-	if Input.is_action_pressed("click"):
+var thrusting := false
+
+func _physics_process(delta: float) -> void:
+	thrusting = Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT)
+	if thrusting:
+		$Fire.visible = true
+	else:
+		$Fire.visible = false
+	$Body.visible = true
+	if thrusting:
 		var dir = (get_global_mouse_position() - global_position).normalized()
-		state.apply_central_force(dir * THRUST)
+		apply_central_force(dir * THRUST)
+		$Fire.rotation_degrees = 150.9 + rad_to_deg(dir.angle()) + 90
 		print(position)
